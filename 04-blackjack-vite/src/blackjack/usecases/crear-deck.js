e4e17eb76128de14badf7708f77fd5d91d2a0c16
Raul Moscoso <raul.moscoso84@gmail.com>
2022-12-03 20:55:22 -0600

    refactorizacion de codigo

---
+++ 04-blackjack-vite/src/blackjack/usecases/crear-deck.js
@@ -0,0 +1,19 @@
+ import _ from 'underscore';
+ // Esta función crea un nuevo deck
+ export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+ 
+    for( let i = 2; i <= 10; i++ ) {
+        for( let tipo of tiposDeCarta ) {
+            deck.push( i + tipo);
+        }
+    }
+
+    for( let tipo of tiposDeCarta ) {
+        for( let esp of tiposEspeciales ) {
+            deck.push( esp + tipo);
+        }
+    }
+    // console.log( deck );
+    deck = _.shuffle( deck );
+    return deck;
+}
