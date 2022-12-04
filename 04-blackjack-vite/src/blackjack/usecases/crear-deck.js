69e99f29fab45cd2492f3baca455075ab189f71e
Raul Moscoso <raul.moscoso84@gmail.com>
2022-12-04 00:06:18 -0600

    Mejorar la documentacion de JSDoc Comments

--- 04-blackjack-vite/src/blackjack/usecases/crear-deck.js
+++ 04-blackjack-vite/src/blackjack/usecases/crear-deck.js
@@ -1,8 +1,23 @@
  import _ from 'underscore';
 
- export const miNombre = 'Raul';
+//  export const miNombre = 'Raul';
 
- export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+/**
+ * Esta funcion crea un nuevo deck
+ * @param {Array<String>} tiposDeCarta Ejemplos: ['C','D','H','S']
+ * @param {Array<String>} tiposEspeciales Ejemplos: ['A','J','Q','K'] 
+ * @returns {Array<String>} retorna un nuevo deck de cartas
+ */
+
+export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+
+    if ( !tiposDeCarta || tiposDeCarta.length === 0 ) 
+        throw new Error('TiposDeCarta es obligatorio como un arreglo de string');
+
+    if ( !tiposEspeciales || tiposEspeciales.length === 0 ) 
+        throw new Error('TiposEspeciales es obligatorio como un arreglo de string');
+
+
     let deck = [];
     for( let i = 2; i <= 10; i++ ) {
         for( let tipo of tiposDeCarta ) {
