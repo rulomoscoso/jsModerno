619096c78ea75ea355446b22bcecdd13b9983120
Raul Moscoso <raul.moscoso84@gmail.com>
2022-12-06 23:13:34 -0600

    Metodos que se ocupan del store

--- 05-todo-app/src/store/todo.store.js
+++ 05-todo-app/src/store/todo.store.js
@@ -20,6 +20,44 @@ const initStore = () => {
     console.log('InitStore 🥑');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    throw new Error('Not implemented');
+}
+
+const toggleTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+const setFilter = ( newFilter = filters.All ) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
+
 export default {
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
