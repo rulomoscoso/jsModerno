82829ea96ceeca790d2fff7ce4ac7cf0c1551291
Raul Moscoso <raul.moscoso84@gmail.com>
2023-01-11 22:39:29 -0600

    Borrar todos los TODOs completados

--- 05-todo-app/src/todos/app.js
+++ 05-todo-app/src/todos/app.js
@@ -3,6 +3,7 @@ import todoStore, {  } from "../store/todo.store";
 import { renderTodos } from './use-cases';
 
 const elementIDs = {
+    ClearCompleted: '.clear-completed',
     TodoList: '.todo-list',
     NewTodoInput: '#new-todo-input',
 }
@@ -27,6 +28,7 @@ export const App = ( elementId) => {
     // Referencias HTML
     const newDescriptionInput = document.querySelector( elementIDs.NewTodoInput );
     const todoListUL = document.querySelector( elementIDs.TodoList );
+    const clearCompletedButton = document.querySelector( elementIDs.ClearCompleted );
 
     // Listeners
     newDescriptionInput.addEventListener('keyup', (event) => {
@@ -50,6 +52,11 @@ export const App = ( elementId) => {
         if( !element || !isDestroyElement ) return;
 
         todoStore.deleteTodo(element.getAttribute('data-id'));
+        displayTodos();
+    });
+
+    clearCompletedButton.addEventListener('click', () => {
+        todoStore.deleteCompleted();
         displayTodos();
     });
 
